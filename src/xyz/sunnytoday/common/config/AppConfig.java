1f903fa160db5de9f0f16b407dca68423a8ae6ce
SonHyunBin <a01047290225@gmail.com>
2021-10-11 18:19:09 +0900

    test commit

--- src/xyz/sunnytoday/common/config/AppConfig.java
+++ src/xyz/sunnytoday/common/config/AppConfig.java
@@ -1,14 +1,13 @@
 package xyz.sunnytoday.common.config;
 
-import xyz.sunnytoday.common.repository.AppKeyRepository;
-import xyz.sunnytoday.common.repository.ForecastRepository;
-import xyz.sunnytoday.common.task.TaskScheduler;
-import xyz.sunnytoday.common.task.TaskConfig;
-
-import java.io.IOException;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+
+import xyz.sunnytoday.common.repository.AppKeyRepository;
+import xyz.sunnytoday.common.repository.ForecastRepository;
+import xyz.sunnytoday.common.task.TaskConfig;
+import xyz.sunnytoday.common.task.TaskScheduler;
 
 public class AppConfig {
     private static AppConfig instance;
