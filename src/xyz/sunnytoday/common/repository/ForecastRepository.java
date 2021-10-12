afc0989d688440b6a88ff20405f8878618bcb4dd
pjtae0328 <pjtae0328@gmail.com>
2021-10-12 11:50:56 +0900

    ForecastService

--- src/xyz/sunnytoday/common/repository/ForecastRepository.java
+++ src/xyz/sunnytoday/common/repository/ForecastRepository.java
@@ -1,13 +1,19 @@
 package xyz.sunnytoday.common.repository;
 
+import xyz.sunnytoday.service.face.ForecastService;
+import xyz.sunnytoday.service.impl.ForecastServiceImpl;
+
+import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 
 public class ForecastRepository {
-    private final Map<String, Region> foreCastMap;
+    private final Map<String, Region> forecastMap;
+    private final ForecastService forecastService = new ForecastServiceImpl();
     private String lastShortTermForecastVersion;
 
     public ForecastRepository(Map<String, Region> foreCastMap) {
-        this.foreCastMap = foreCastMap;
+        this.forecastMap = foreCastMap;
     }
 
     public String getLastShortTermForecastVersion() {
@@ -16,5 +22,19 @@ public class ForecastRepository {
 
     public void setLastShortTermForecastVersion(String lastShortTermForecastVersion) {
         this.lastShortTermForecastVersion = lastShortTermForecastVersion;
+    }
+
+    public List<Forecast> getShortTermForecast(String city) throws IOException {
+        if (!this.forecastMap.containsKey(city)) {
+            throw new IllegalArgumentException("도시이름이 정확하지 않습니다.");
+        }
+
+        Region region = this.forecastMap.get(city);
+        if(region.needShortTermForecastUpdate()) {
+            //업데이트 로직
+            forecastService.requestGeoLocationData(region);
+        }
+
+        return region.getShortTermForecasts();
     }
 }
