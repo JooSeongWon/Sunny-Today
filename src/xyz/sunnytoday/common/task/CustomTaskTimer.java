a6d6e29dce6a4b3c1fa59ddeec49b85ac01c7614
pjtae0328 <pjtae0328@gmail.com>
2021-10-17 03:31:51 +0900

    메일전송 안정화

--- src/xyz/sunnytoday/common/task/CustomTaskTimer.java
+++ src/xyz/sunnytoday/common/task/CustomTaskTimer.java
@@ -3,9 +3,11 @@ package xyz.sunnytoday.common.task;
 public class CustomTaskTimer extends TaskTimer {
     private final Runnable runnable;
 
-    public CustomTaskTimer(int interval, Runnable runnable) {
+    public CustomTaskTimer(int interval, Runnable runnable, boolean isFirstTimeStart) {
         super(interval);
         this.runnable = runnable;
+        //첫번째 실행 무시합니다.
+        if(!isFirstTimeStart) super.setNoFirstTimeStart();
     }
 
     @Override
